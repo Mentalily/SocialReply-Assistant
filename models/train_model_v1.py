dd42f546667bba7efd8e010a848938c0ebeed976
Mentalily <3339672417@qq.com>
2026-01-06 21:46:30 +0800

    模型训练过程注释

--- models/train_model_v1.py
+++ models/train_model_v1.py
@@ -42,10 +42,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_
 # max_features=5000 表示只保留最重要的5000个词，防止数据量太大
 vectorizer = TfidfVectorizer(max_features=20000, ngram_range=(1, 2)) # 同时提取 1-gram (单个词) 和 2-gram (双词组合) 作为特征
 
+# x_train_vec是张巨大的表，行是样本句子，列是特征词，数据项是0-1的TF-IDF分数（特征词在样本句子中的重要程度）
+# fit_transform基于训练数据产出规则（fit: 学习/你和），并根据规则转为数据
 X_train_vec = vectorizer.fit_transform(X_train)
+# transform使用刚才学习拟合的结果得到数字
 X_test_vec = vectorizer.transform(X_test)
 
+# C=1.0 正则化强度的倒数，C越大可能导致过拟合，C太小可能欠拟合
+# liblinear 是专门为大规模稀疏数据（比如要处理的 20,000 维文本特征）设计的算法。在文本分类里，它通常跑得飞快且效果好。
+# max_iter=1000 (最大迭代次数): 找最优权重的过程是慢慢磨出来的（梯度下降）
+# 底层算法：z = (w1*x1)+(w2*x2)+...+bias. x1是特征值（TF-IDF计算结果），w1是权重，迭代多次不断修正得到的较优权重
+# 再通过sigmoid函数映射到 0-1 区间
 model = LogisticRegression(C=1.0, solver='liblinear', max_iter=1000)
+# 根据答案不断修改权重以尽可能接近答案
 model.fit(X_train_vec, y_train)
 
 # ================== 3. 评估与保存 ==================
